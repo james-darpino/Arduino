110731d3c01fba17c0d826e5d953fa9e955dfda5
James D'Arpino <31546795+james-darpino@users.noreply.github.com>
2021-08-09 15:37:13 -0400

    Update Lab_2.ino

--- Lab_2.ino
+++ Lab_2.ino
@@ -1,1 +1,37 @@
-int switchState = 0;void setup() {  pinMode(3, OUTPUT);  pinMode(4, OUTPUT);  pinMode(5, OUTPUT);  pinMode(2, INPUT);  }void loop() {  switchState = digitalRead(2);  if (switchState == LOW) {    //the button is not pressed            digitalWrite(7, HIGH); //green LED   // digitalWrite(4, LOW); //red LED   // digitalWrite(5, LOW); //red LED  }    else { //button is pressed    digitalWrite(3, LOW); //green LED    digitalWrite(4, LOW); //red LED    digitalWrite(5, HIGH); //red LED        delay(25); //wait for a quarter second    //toggle LEDs    digitalWrite(4, HIGH); //red LED    digitalWrite(5, LOW); //red LED    delay(25); // wait for a quarter second      }  } //go back to the beginning of the loop
+int switchState = 0;
+
+
+
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);  
+}
+
+void loop() {
+  switchState = digitalRead(2);
+  if (switchState == LOW) {
+    //the button is not pressed
+    
+    
+    digitalWrite(7, HIGH); //green LED
+   // digitalWrite(4, LOW); //red LED
+   // digitalWrite(5, LOW); //red LED
+  }
+  
+  else { //button is pressed
+    digitalWrite(3, LOW); //green LED
+    digitalWrite(4, LOW); //red LED
+    digitalWrite(5, HIGH); //red LED
+    
+    delay(25); //wait for a quarter second
+    //toggle LEDs
+    digitalWrite(4, HIGH); //red LED
+    digitalWrite(5, LOW); //red LED
+    delay(25); // wait for a quarter second
+    
+  }
+  
+} //go back to the beginning of the loop
